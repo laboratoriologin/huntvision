466ff08380f6b85c8d6f7a043bff68835472bcba
Marcelo <marcelomagalhaes31@gmail.com>
2015-06-25 11:06:10 -0300

    ...

---
+++ iOS/HuntVision/HuntVision/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HuntVision
+//
+//  Created by Login Informatica on 25/06/15.
+//  Copyright (c) 2015 Login Informatica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
