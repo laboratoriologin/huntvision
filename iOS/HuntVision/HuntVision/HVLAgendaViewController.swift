bcf53fb918f9095ac18dd81704e30341743d15d0
Marcelo <marcelomagalhaes31@gmail.com>
2015-07-20 16:56:44 -0300

    ...

---
+++ iOS/HuntVision/HuntVision/HVLAgendaViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HVLAgendaViewController.swift
+//  HuntVision
+//
+//  Created by Login Informatica on 20/07/15.
+//  Copyright (c) 2015 Login Informatica. All rights reserved.
+//
+
+import UIKit
+
+class HVLAgendaViewController: UITableViewController {
+
+    var agendas = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return agendas.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+}
---
+++ iOS/HuntVision/HuntVision/HVLHomeController.swift
@@ -0,0 +1,12 @@
+//
+//  HVLHomeController.swift
+//  HuntVision
+//
+//  Created by Login Informatica on 20/07/15.
+//  Copyright (c) 2015 Login Informatica. All rights reserved.
+//
+
+
+class HVLHomeController: UIViewController {
+
+}
